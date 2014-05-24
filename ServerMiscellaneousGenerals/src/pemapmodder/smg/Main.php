e5baeebae99ddf43aa1e336684a2c826f7b7163c
mcpesrccode <pemapmodder1970@gmail.com>
2014-05-24 18:36:20 +0800

    A lot of SMG things

--- ServerMiscellaneousGenerals/src/pemapmodder/smg/Main.php
+++ ServerMiscellaneousGenerals/src/pemapmodder/smg/Main.php
@@ -2,28 +2,71 @@
 
 namespace pemapmodder\smg;
 
+use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender as Issuer;
 use pocketmine\event\EventPriority as EP;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase as ParentClass;
+use pocketmine\utils\Config;
 
 class Main extends ParentClass implements Listener{
-	const PERMANENT = 0;
+	const REGPEN	= 1;
+	const REGBAN	= 3;
+	const PERMBAN	= 15;
 	
-	const IMPROPER_CHAT	= 0b00000111;
-	const SPAM			= 0b00000001;
-	const SWEAR			= 0b00000010;
-	const STAFF_IMPOSE	= 0b00000100;
-	const MOD_USE		= 0b11110000;
-	const CLIMB_MOD		= 0b00010000;
-	const FLY_MOD		= 0b00100000;
-	const JUMP_MOD		= 0b01000000;
-	const SPRINT_MOD	= 0b10000000;
+	const SPAM			= 0b000000000001; // 1
+	const HARRASS		= 0b000000000010; // 2
+	const SWEAR			= 0b000000000100; // 4
+	const STAFF_IMPOSE	= 0b000000001000; // 8
+	const CLIMB_MOD		= 0b000000010000; // 16
+	const FLY_MOD		= 0b000000100000; // 32
+	const JUMP_MOD		= 0b000001000000; // 64
+	const SPRINT_MOD	= 0b000010000000; // 128
+	const GLITCH_USE	= 0b000100000000; // 256
+	const IMPROPER_CHAT	= 0b001000000000; // 512
+	const MOD_USE		= 0b000011110000; // 235
+	
+	const ADMIN = "admin";
+	const MOD_GLOB = "global moderator";
+	const MOD_SEC = "sectional moderator";
+	const NORM = "player";
+	
+	public $list;
+	public $penalties = [];
 	
 	public function onEnable(){
 		$this->actionLogger = new ActionLogger($this);
+		$this->list = new BanList($this->getDataFolder()."ban-list.json");
+		$this->ranks = new Config($this->getDataFolder()."config.yml", Config::YAML, [
+			"admin" => ["lambo", "spyduck", "pemapmodder"],
+			"mods" => [
+				"global" => ["sean_m"],
+				"sectional" => [
+					"trollofmc" => ["world_pvp", "world_spleef"],
+				]
+			]
+		]);
+	}
+	public function getRank(Player $player){
+		$name = strtolower($player->getName());
+		foreach($this->ranks->get("admin") as $member){
+			if(strtolower($member) === $name){
+				return self::ADMIN;
+			}
+		}
+		foreach($this->ranks->get("mods")["global"] as $member){
+			if(strtolower($member) === $name){
+				return self::MOD_GLOB;
+			}
+		}
+		foreach($this->ranks->get("mods")["sectional"] as $mod => $worlds){
+			if($mod === $name){
+				return self::MOD_SEC;
+			}
+		}
+		return self::NORM;
 	}
 	public function onCommand(Issuer $isr, Command $cmd, $lbl, array $args){
 		switch($cmd){
@@ -32,10 +75,63 @@ class Main extends ParentClass implements Listener{
 					return false;
 				}
 				$player = array_shift($args);
-				$this->submitReport(
+				$details = implode(" ", $args);
+				break;
 		}
 	}
-	public function banIP($hours = self::PERMANENT, $reason){
-		$this->getServer()->get;
+	public function evalFlags($flags){
+		$out = [];
+		// reasons
+		if($flags & self::SPAM){
+			$out[] = "spam";
+		}
+		if($flags & self::HARRASS){
+			$out[] = "harrassing other players";
+		}
+		if($flags & self::SWEAR){
+			$out[] = "swearing";
+		}
+		if($flags & self::STAFF_IMPOSE){
+			$out[] = "staff imposement";
+		}
+		if($flags & self:CLIMB_MOD){
+			$out[] = "climbing mod usage";
+		}
+		if($flags & self::FLY_MOD){
+			$out[] = "flying mod usage";
+		}
+		if($flags & self::JUMP_MOD){
+			$out[] = "super jump mod usage";
+		}
+		if($flags & self::SPRINT_MOD){
+			$out[] = "using walk faster/sprinting mod";
+		}
+		if($flags & self::GLITCH_USE){
+			$out[] = "improper usage of glitches or bugs";
+		}
+		if($flags & self::IMPROPER_CHAT){
+			$out[] = "improper chat behaviours";
+		}
+		// actions
+		$actions = 0;
+		if($flags & self::MOD_USE){
+			$actions += self::REGBAN;
+		}
+		if($flags & self::SPAM){
+			$action += self::REGBAN;
+		}
+		if($flags & self::HARRASS){
+			$action += self::REGPEN;
+		}
+		if($flags & self::SWEAR){
+			$actions += self::REGPEN;
+		}
+		if($flags & self::STAFF_IMPOSE){
+			$actions += self::REGBAN;
+		}
+		return [$out, $actions];
+	}
+	public static function get(){
+		return Server::getInstance()->getPluginManager()->getPlugin("ServerMiscellaneousGenerals");
 	}
 }
--- ServerMiscellaneousGenerals/src/pemapmodder/smg/Penalty.php
+++ ServerMiscellaneousGenerals/src/pemapmodder/smg/Penalty.php
@@ -2,8 +2,37 @@
 
 namespace pemapmodder\smg;
 
-class Penalty{
-	public function __construct($server, $target, $reasons, $extraData){
-		$this->server = $server;
+use pocketmine\Player;
+use pocketmine\Server;
+
+class Penalty extends PluginTask{
+	public static $pid = 0;
+	public $cancelled = false;
+	public $id;
+	public static function add(Player $issuer, Player $target, $flags, $seconds = 15, $extraData = false){
+		$penalty = new static($issuer, $target, $flags, $seconds, $extraData)
+		Server::getInstance()->getScheduler()->scheduleDelayedTask($penalty, $seconds * 15);
+		Main::get()->penalties[$penalty->id] = $penalty;
+	}
+	public function __construct(Player $issuer, Player $target, $reasons, $seconds = 15, $extraData = false){
+		parent::__construct(Main::get());
+		$this->server = Server::getInstance();
+		$this->id = self::$pid++;
+		$this->inetAddress = $target->getAddress();
+		$target->sendMessage("You are given a penalty by ".Main::get()->getRank($issuer)." ".$issuer->getDisplayName()." for the following reasons:");
+		$target->sendMessage(implode(", ", Main::get()->evalFlags($reasons)[0]));
+		if($extraData){
+			$target->sendMessage("Extra information from the penalty issuer: $extraData");
+		}
+		$target->sendMessage("You can raise an objection to this ban in 15 seconds.");
+		$target->sendMessage("If your objection is not accepted, you can create a ban appeal on penalty ID {$this->id} on the forums.");
+		$this->action = Main::get()->evalFlags($reasons)[1];
+		$target->sendMessage("This penalty gives you {$this->action} ban points.");
+	}
+	public function onRun($ticks){
+		Main::get()->list->warn($this->inetAddress, $this->action);
+	}
+	public static function get($pid){
+		return @Main::get()->penalties[$pid];
 	}
 }
